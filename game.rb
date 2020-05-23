91d305e5076019d713deac7a1221c480f8d9b70e
null null <demimatar@gmail.com>
2020-05-23 04:20:45 +0000

    To do: create story for choices made pill & bottle

--- game.rb
+++ game.rb
@@ -3,6 +3,22 @@ require_relative 'test'
 class Game
   include Ui
 
+  def initialize
+    @menu = [
+    'Explore the room',
+    'Use the syringe',
+    'Drink from the bottle',
+    'Swallow the pill capsule',
+    'Go back to sleep'
+    ]
+
+    @menu_loop = true
+    @choices_made = {
+    bottle: false,
+    pill: false
+    }
+  end
+
   def play
     introduction
     introduction2
@@ -48,23 +64,16 @@ class Game
   end
 
   def main_menu
-    menu = [
-    'Explore the room',
-    'Use the syringe',
-    'Drink from the bottle',
-    'Swallow the pill capsule',
-    'Go back to sleep'
-    ]
+    @menu_loop = true
 
-    menu_loop = true
-
-    while menu_loop == true
+    while @menu_loop == true
+      @menu_loop = true
       puts 'What would you like to do?'
-      sleep(1.5)
-      display_menu_options(menu)
+      sleep(0.5)
+      display_menu_options(@menu)
 
       first_story_fork = ask_question('ENTER THE NUMBER ONLY')
-      menu_loop = respond_to_first_story_fork(first_story_fork)
+      respond_to_first_story_fork(first_story_fork)
     end
   end
 
@@ -81,42 +90,65 @@ class Game
     end
   end
 
-  def respond_to_first_story_fork(first)
-    if first == '1'
+  def respond_to_first_story_fork(first)  
+    try = @menu.index('Try for the arch again')
+    explore = @menu.index('Explore the room')
+    use = @menu.index('Use the syringe')
+    drink = @menu.index('Drink from the bottle')
+    swallow = @menu.index('Swallow the pill capsule')
+    go = @menu.index('Go back to sleep')
+
+    if !explore.nil? && first == (explore + 1).to_s
       explore_the_room_pt1
-    elsif first == '2'
+    elsif !use.nil? && first == (use + 1).to_s
       use_the_syringe
-    elsif first == '3'
+    elsif !drink.nil? && first == (drink + 1).to_s
       drink_from_the_bottle
-    elsif first == '5'
-      return false
+    elsif !swallow.nil? && first == (swallow + 1).to_s
+      swallow_the_pill  
+    elsif !go.nil? && first == (go + 1).to_s
+      @menu_loop = false
+    elsif !try.nil? && first == (try + 1).to_s
+      # try_for_the_arch_again
     else
-      puts 'Error. Try again.'
+      puts 'Error. Try again.'   
     end
   end
 
   def explore_the_room_pt1
+  #   elsif !@choices_made[:pill] && @choices_made[:bottle]
+  #   elsif @choices_made[:pill] && @choices_made[:bottle]
+  #   else
     puts tab + 'story for what happens if you click explore the room'
     puts tab + 'storyyyyyyy'
     puts ''
 
-    explore_the_room_pt2 = ask_question('Shall you continue forward?')
-    respond_to_explore_the_room_pt2(explore_the_room_pt2.downcase)
+    loop do
+      explore_the_room_pt2 = ask_question('Shall you continue forward?')
+      break if respond_to_explore_the_room_pt2(explore_the_room_pt2.downcase) != false
+    end  
+  end
+
+  def if_not_ready
+    puts tab + 'story for if you hit continue after hitting explore the room'
+    sleep(5)
+    puts "Press ENTER to continue"
+    gets
+    @menu_loop = false
   end
 
   def respond_to_explore_the_room_pt2(second)
     if second == 'yes'
-      puts tab + 'story for if you hit continue after hitting explore the room'
-      sleep(5)
-      puts "Press ENTER to continue"
-      gets
-      return false
+      if_not_ready
+      true
     elsif second == 'no'
       puts tab + 'story for if you do not continue after hitting explore the room'
       sleep(4)
-      return true
+      @menu[0] = 'Try for the arch again'
+      true
     else
-      puts 'Error. Try again'  
+      puts 'Error. Try again'
+      false  
     end
   end 
 
@@ -125,12 +157,32 @@ class Game
     sleep(5)
     puts 'Press ENTER to continue'
     gets
-    return false
+    @menu_loop = false
   end
 
   def drink_from_the_bottle
     puts tab + 'story for what happens if you drink from the bottle.'
-    return true
+    sleep(5)
+    @menu.delete('Drink from the bottle')
+    @choices_made[:bottle] = true
+    true
   end
 
+  def swallow_the_pill
+    puts tab + 'story for what happens if you swallow the pill.'
+    sleep(5)
+    @menu.delete('Swallow the pill capsule')
+    @choices_made[:pill] = true
+    true
+  end
+
+  # def try_for_the_arch_again
+  #   elsif !@choices_made[:pill] && @choices_made[:bottle]
+  #   elsif @choices_made[:pill] && @choices_made[:bottle]
+  #   else
+  #     if_not_ready
+
+  #   end
+
+  # end
 end
